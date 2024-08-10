19cbb4ea89a82e50f239a686b8c02be17b353db2
Administrator <admin@example.com>
2024-08-10 16:34:41 +0800

    feat: 诚信乡村地区

---
+++ app/Models/RuralRegion.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * App\Models\RuralRegion
+ *
+ * @property int $id
+ * @property string $name 地区名称
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|RuralRegion newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|RuralRegion newQuery()
+ * @method static \Illuminate\Database\Query\Builder|RuralRegion onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|RuralRegion query()
+ * @method static \Illuminate\Database\Eloquent\Builder|RuralRegion whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RuralRegion whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RuralRegion whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|RuralRegion whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|RuralRegion withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|RuralRegion withoutTrashed()
+ * @mixin \Eloquent
+ */
+class RuralRegion extends BaseModel
+{
+}
