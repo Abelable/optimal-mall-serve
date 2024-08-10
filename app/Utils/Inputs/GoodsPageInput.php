0d13a1205f601aec3ebc48755d3f9119b8908be8
Administrator <admin@example.com>
2024-08-10 12:10:30 +0800

    feat: 商品列表

--- app/Utils/Inputs/GoodsPageInput.php
+++ app/Utils/Inputs/GoodsPageInput.php
@@ -6,14 +6,12 @@ class GoodsPageInput extends PageInput
 {
     public $goodsIds;
     public $categoryId;
-    public $sort;
 
     public function rules()
     {
         return array_merge(parent::rules(), [
             'goodsIds' => 'array',
             'categoryId' => 'integer|digits_between:1,20',
-            'sort' => 'string',
         ]);
     }
 }
