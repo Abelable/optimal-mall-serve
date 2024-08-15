0a683d44e43cbdb22f6c9f1eb42c4fef653f2d42
Administrator <admin@example.com>
2024-08-15 14:52:06 +0800

    feat: 注册绑定上下级

---
+++ app/Services/RelationService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Relation;
+
+class RelationService extends BaseService
+{
+    public function banding($superiorId, $fanId)
+    {
+        $relation = Relation::new();
+        $relation->superior_id = $superiorId;
+        $relation->fan_id = $fanId;
+        $relation->save();
+        return $relation;
+    }
+
+    public function getRelationByFanId($fanId, $columns = ['*'])
+    {
+        return Relation::query()->where('fan_id', $fanId)->first($columns);
+    }
+}
