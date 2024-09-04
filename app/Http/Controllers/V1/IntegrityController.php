da33b86986fa15f37bb70ec10c47e5f58aa57b71
Administrator <admin@example.com>
2024-09-04 17:01:46 +0800

    feat: 诚信臻品商品列表

--- app/Http/Controllers/V1/IntegrityController.php
+++ app/Http/Controllers/V1/IntegrityController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Goods;
+use App\Services\ActivityService;
+use App\Services\CouponService;
 use App\Services\GoodsService;
 use App\Services\IntegrityBannerService;
 use App\Services\IntegrityGoodsService;
@@ -20,10 +23,25 @@ class IntegrityController extends Controller
     public function goodsList()
     {
         $goodsIds = IntegrityGoodsService::getInstance()->getGoodsList(['goods_id'])->pluck('goods_id')->toArray();
+        $activityList = ActivityService::getInstance()
+            ->getActivityListByGoodsIds($goodsIds, ['status', 'name', 'start_time', 'end_time', 'goods_id', 'followers', 'sales'])
+            ->keyBy('goods_id');
+        $groupedCouponList = CouponService::getInstance()
+            ->getCouponListByGoodsIds($goodsIds, ['goods_id', 'name', 'denomination', 'type', 'num_limit', 'price_limit'])
+            ->groupBy('goods_id');
         $goodsList = GoodsService::getInstance()->getGoodsListByIds($goodsIds);
+        $list = $goodsList->map(function (Goods $goods) use ($activityList, $groupedCouponList) {
+            $activity = $activityList->get($goods->id);
+            $goods['activityInfo'] = $activity;
+
+            $couponList = $groupedCouponList->get($goods->id);
+            $goods['couponList'] = $couponList ?: [];
+
+            return $goods;
+        });
 
         // todo 商品列表存缓存
 
-        return $this->success($goodsList);
+        return $this->success($list);
     }
 }
