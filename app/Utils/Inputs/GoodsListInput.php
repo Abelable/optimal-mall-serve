1b11f3012567cfdf111e3955084bb0ed66bd193f
Administrator <admin@example.com>
2024-09-09 14:50:38 +0800

    feat: 诚信乡村商品

--- app/Utils/Inputs/GoodsListInput.php
+++ app/Utils/Inputs/GoodsListInput.php
@@ -4,11 +4,13 @@ namespace App\Utils\Inputs;
 
 class GoodsListInput extends BaseInput
 {
+    public $regionId;
     public $goodsIds;
 
     public function rules()
     {
         return [
+            'regionId' => 'integer|digits_between:1,20',
             'goodsIds' => 'required|array|min:1'
         ];
     }
