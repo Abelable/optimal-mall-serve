0f7905c7c97ce97b1541f228e4e2d6fa8b951af8
Administrator <admin@example.com>
2024-10-04 22:36:39 +0800

    feat: 佣金提现

---
+++ app/Models/Withdrawal.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+/**
+ * App\Models\Withdrawal
+ *
+ * @property int $id
+ * @property int $user_id 用户id
+ * @property int $type 佣金类型：1-商品自购佣金；2-商品分享佣金；3-礼包佣金
+ * @property float $withdraw_amount 提现金额
+ * @property float $tax_fee 税费
+ * @property float $handling_fee 手续费
+ * @property float $actual_amount 实际到账金额
+ * @property int $path 提现方式：1-微信；2-银行卡
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Withdrawal onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereActualAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereHandlingFee($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereTaxFee($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Withdrawal whereWithdrawAmount($value)
+ * @method static \Illuminate\Database\Query\Builder|Withdrawal withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Withdrawal withoutTrashed()
+ * @mixin \Eloquent
+ */
+class Withdrawal extends BaseModel
+{
+}
