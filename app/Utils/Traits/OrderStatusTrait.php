d1b4ff15320ca4fa0a563d7c6cedf123b00e4516
funguy <1209683835@qq.com>
2024-12-27 16:14:28 +0800

    feat: 导出订单状态下可退款

--- app/Utils/Traits/OrderStatusTrait.php
+++ app/Utils/Traits/OrderStatusTrait.php
@@ -62,7 +62,7 @@ trait OrderStatusTrait
         'export' => [OrderEnums::STATUS_PAY],
         'ship' => [OrderEnums::STATUS_PAY, OrderEnums::STATUS_EXPORTED],
         'confirm' => [OrderEnums::STATUS_SHIP],
-        'refund' => [OrderEnums::STATUS_PAY, OrderEnums::STATUS_REFUND],
+        'refund' => [OrderEnums::STATUS_PAY, OrderEnums::STATUS_EXPORTED, OrderEnums::STATUS_REFUND],
         // 同意退款
         'agreerefund' => [OrderEnums::STATUS_REFUND],
         'comment' => [
