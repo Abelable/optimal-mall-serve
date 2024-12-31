7bf8eff3f681f3b415f03f4499319dee59fd3acc
funguy <1209683835@qq.com>
2024-12-31 14:19:09 +0800

    feat: 首页金刚区

--- app/Services/VillageFreshGoodsService.php
+++ app/Services/VillageFreshGoodsService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-use App\Models\VillageGrainGoods;
+use App\Models\VillageFreshGoods;
 use App\Utils\Inputs\GoodsListInput;
 use App\Utils\Inputs\PageInput;
 
@@ -10,26 +10,26 @@ class VillageFreshGoodsService extends BaseService
 {
     public function getGoodsPage(PageInput $input, $columns = ['*'])
     {
-        return VillageGrainGoods::query()->orderBy($input->sort, $input->order)->paginate($input->limit, $columns, 'page', $input->page);
+        return VillageFreshGoods::query()->orderBy($input->sort, $input->order)->paginate($input->limit, $columns, 'page', $input->page);
     }
 
     public function getGoodsList($columns = ['*'])
     {
-        return VillageGrainGoods::query()->orderBy('sort', 'desc')->get($columns);
+        return VillageFreshGoods::query()->orderBy('sort', 'desc')->get($columns);
     }
 
     public function getFilterGoodsList(GoodsListInput $input, $columns = ['*'])
     {
-        return VillageGrainGoods::query()->whereIn('goods_id', $input->goodsIds)->get($columns);
+        return VillageFreshGoods::query()->whereIn('goods_id', $input->goodsIds)->get($columns);
     }
 
     public function getGoodsById($id, $columns = ['*'])
     {
-        return VillageGrainGoods::query()->find($id, $columns);
+        return VillageFreshGoods::query()->find($id, $columns);
     }
 
     public function deleteById($id, $columns = ['*'])
     {
-        VillageGrainGoods::query()->where('id', $id)->delete();
+        VillageFreshGoods::query()->where('id', $id)->delete();
     }
 }
