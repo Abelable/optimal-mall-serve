4689d5a11c87192bc6aeccab62aa955018022223
funguy <1209683835@qq.com>
2025-01-06 15:38:13 +0800

    feat: 推广员

--- database/migrations/2024_08_19_152937_create_promoters_table.php
+++ database/migrations/2024_08_19_152937_create_promoters_table.php
@@ -18,7 +18,7 @@ class CreatePromotersTable extends Migration
             $table->integer('user_id')->comment('用户id');
             $table->integer('level')->comment('用户等级：1-乡村推广员，2-乡村组织者C1，3-C2，4-C3，5-乡村振兴委员会');
             $table->integer('scene')->comment('场景值，防串改，与等级对应「等级-场景值」：1-100, 2-201, 3-202, 4-203, 5-300');
-            $table->integer('path')->default(1)->comment('生成路径：1-管理后台添加，2-礼包购买，3-限时活动');
+            $table->integer('path')->comment('生成路径：1-管理后台添加，2-礼包购买，3-限时活动');
             $table->string('goods_ids')->default('')->comment('礼包商品id');
             $table->timestamps();
             $table->softDeletes();
