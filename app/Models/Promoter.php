e2a67573e5d380b96b5336bd0a5f19a4033a86b2
funguy <1209683835@qq.com>
2025-03-03 14:16:23 +0800

    feat: 推广员

--- app/Models/Promoter.php
+++ app/Models/Promoter.php
@@ -11,6 +11,8 @@ namespace App\Models;
  * @property int $scene 场景值，防串改，与等级对应「等级-场景值」：1-100, 2-201, 3-202, 4-203, 5-300
  * @property int $path 生成路径：1-管理后台添加，2-礼包购买，3-限时活动
  * @property string $goods_ids 礼包商品id-用于售后退款删除推广员身份
+ * @property int $promoted_user_number 推广人数
+ * @property float $commission_total 累计佣金
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
  * @property \Illuminate\Support\Carbon|null $deleted_at
@@ -18,12 +20,14 @@ namespace App\Models;
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter newQuery()
  * @method static \Illuminate\Database\Query\Builder|Promoter onlyTrashed()
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereCommissionTotal($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereCreatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereDeletedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereGoodsIds($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereLevel($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter wherePath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Promoter wherePromotedUserNumber($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereScene($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereUpdatedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|Promoter whereUserId($value)
--- database/migrations/2024_08_19_152937_create_promoters_table.php
+++ database/migrations/2024_08_19_152937_create_promoters_table.php
@@ -20,6 +20,8 @@ class CreatePromotersTable extends Migration
             $table->integer('scene')->comment('场景值，防串改，与等级对应「等级-场景值」：1-100, 2-201, 3-202, 4-203, 5-300');
             $table->integer('path')->comment('生成路径：1-管理后台添加，2-礼包购买，3-限时活动');
             $table->string('goods_ids')->default('')->comment('礼包商品id-用于售后退款删除推广员身份');
+            $table->integer('promoted_user_number')->default(0)->comment('推广人数');
+            $table->float('commission_total')->default(0)->comment('累计佣金');
             $table->timestamps();
             $table->softDeletes();
         });
