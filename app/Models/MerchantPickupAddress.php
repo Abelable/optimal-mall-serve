97af59b0865cf6061bf27ad4e3707caa6535bcd6
funguy <1209683835@qq.com>
2025-03-06 15:48:02 +0800

    feat: 商家退货、提货地址

--- app/Models/MerchantPickupAddress.php
+++ app/Models/MerchantPickupAddress.php
@@ -2,6 +2,33 @@
 
 namespace App\Models;
 
+/**
+ * App\Models\MerchantPickupAddress
+ *
+ * @property int $id
+ * @property int $merchant_id 商家id
+ * @property string $longitude 提货点经度
+ * @property string $latitude 提货点纬度
+ * @property string $address_detail 提货点地址
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress newQuery()
+ * @method static \Illuminate\Database\Query\Builder|MerchantPickupAddress onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress query()
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress whereAddressDetail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress whereLatitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress whereLongitude($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress whereMerchantId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantPickupAddress whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|MerchantPickupAddress withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|MerchantPickupAddress withoutTrashed()
+ * @mixin \Eloquent
+ */
 class MerchantPickupAddress extends BaseModel
 {
 }
--- app/Models/MerchantRefundAddress.php
+++ app/Models/MerchantRefundAddress.php
@@ -2,6 +2,33 @@
 
 namespace App\Models;
 
+/**
+ * App\Models\MerchantRefundAddress
+ *
+ * @property int $id
+ * @property int $merchant_id 商家id
+ * @property string $consignee_name 收件人姓名
+ * @property string $mobile 手机号
+ * @property string $address_detail 收件地址
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress newQuery()
+ * @method static \Illuminate\Database\Query\Builder|MerchantRefundAddress onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress query()
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress whereAddressDetail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress whereConsigneeName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress whereMerchantId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress whereMobile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|MerchantRefundAddress whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|MerchantRefundAddress withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|MerchantRefundAddress withoutTrashed()
+ * @mixin \Eloquent
+ */
 class MerchantRefundAddress extends BaseModel
 {
 }
