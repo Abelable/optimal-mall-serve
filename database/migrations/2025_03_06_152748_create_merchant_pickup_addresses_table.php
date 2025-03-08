dd1b70b9c797c21db58e56d1b9658e2831666059
funguy <1209683835@qq.com>
2025-03-08 14:17:25 +0800

    feat: 商家退货、提货地址

--- database/migrations/2025_03_06_152748_create_merchant_pickup_addresses_table.php
+++ database/migrations/2025_03_06_152748_create_merchant_pickup_addresses_table.php
@@ -16,7 +16,7 @@ class CreateMerchantPickupAddressesTable extends Migration
         Schema::create('merchant_pickup_addresses', function (Blueprint $table) {
             $table->id();
             $table->integer('merchant_id')->comment('商家id');
-            $table->string('name')->comment('提货点名称');
+            $table->string('name')->default('')->comment('提货点名称');
             $table->string('time_frame')->default('')->comment('提货时间范围');
             $table->string('address_detail')->comment('提货点地址');
             $table->decimal('longitude', 9, 6)->comment('提货点经度');
