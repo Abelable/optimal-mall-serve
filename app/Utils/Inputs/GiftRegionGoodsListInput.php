ecc1e0dfad3b2d69c751db20ad84a384038d62ed
funguy <1209683835@qq.com>
2025-03-19 19:41:03 +0800

    feat: 浙江产业带

--- app/Utils/Inputs/GiftRegionGoodsListInput.php
+++ app/Utils/Inputs/GiftRegionGoodsListInput.php
@@ -5,11 +5,13 @@ namespace App\Utils\Inputs;
 class GiftRegionGoodsListInput extends RegionGoodsListInput
 {
     public $type;
+    public $goodsIds;
 
     public function rules()
     {
         return array_merge(parent::rules(), [
             'type' => 'required|integer|in:1,2',
+            'goodsIds' => 'required|array|min:1'
         ]);
     }
 }
