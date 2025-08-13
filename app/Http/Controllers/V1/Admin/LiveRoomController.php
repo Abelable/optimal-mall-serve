7b0588d65cd3bb58a2e08e8e55b38185b2e6ecbc
funguy <1209683835@qq.com>
2025-08-13 13:41:18 +0800

    feat: 直播

--- app/Http/Controllers/V1/Admin/LiveRoomController.php
+++ app/Http/Controllers/V1/Admin/LiveRoomController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\V1\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\LiveRoom;
 use App\Services\LiveRoomService;
+use App\Services\UserService;
 use App\Utils\Inputs\PageInput;
 
 class LiveRoomController extends Controller
@@ -15,6 +17,19 @@ class LiveRoomController extends Controller
         /** @var PageInput $input */
         $input = PageInput::new();
         $page = LiveRoomService::getInstance()->adminPageList($input);
-        return $this->successPaginate($page);
+        $liveRoomList = collect($page->items());
+
+        $userIds = $liveRoomList->pluck('user_id')->toArray();
+        $userList = UserService::getInstance()->getListByIds($userIds)->keyBy('id');
+
+        $list = $liveRoomList->map(function (LiveRoom $liveRoom) use ($userList) {
+            $user = $userList->get($liveRoom->user_id);
+            $liveRoom['userInfo'] = $user;
+            unset($liveRoom->user_id);
+
+            return $liveRoom;
+        });
+
+        return $this->success($this->paginate($page, $list));
     }
 }
